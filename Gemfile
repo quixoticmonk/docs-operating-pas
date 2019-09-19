d42221b824c9ce367227b8d2c0e4db09589e30b1
Miranda Limonczenko <mlimonczenko@pivotal.io>
2019-09-19 14:43:30 -0700

    remove old stuff from prev repo

--- Gemfile
+++
@@ -1,3 +0,0 @@
-source 'http://rubygems.org'
-gem 'therubyracer'
-gem 'bookbindery', "9.12.0"
--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Bring up the Docs at http://localhost:4567
-bundle exec bookbinder bind local
-cd final_app
-bundle
-bundle exec rackup
